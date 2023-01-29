67dc089197c6d6688b7f52f50f6e46bbde398016
Ljsung6 <ljsung6@naver.com>
2023-01-29 22:13:49 +0900

    문제풀이 11번

--- 과제계정.sql
+++ 과제계정.sql
@@ -73,4 +73,18 @@ SELECT
         STUDENT_ADDRESS LIKE '%전주%'
   AND   ABSENCE_YN = 'N';
   
-  
+--1. 영어영문학과(학과코드 002) 학생들의 학번과 이름, 입학 년도를 입학 년도가 빠른
+--순으로 표시하는 SQL 문장을 작성하시오.( 단, 헤더는 "학번", "이름", "입학년도" 가
+--표시되도록 한다.)
+
+SELECT
+        STUDENT_NAME 이름
+      , STUDENT_NO 학번
+      , TO_CHAR(ENTRANCE_DATE,'YYYY-MM-DD') 입학년도
+    FROM TB_STUDENT
+    WHERE DEPARTMENT_NO = 002 ORDER BY 3;
+    
+
+   
+    
+
