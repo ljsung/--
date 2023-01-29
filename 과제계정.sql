fb50cacc797263348e0ee8ee9cb87693fd7e83e1
Ljsung6 <ljsung6@naver.com>
2023-01-29 20:32:43 +0900

    2번문제

--- 과제계정.sql
+++ 과제계정.sql
@@ -5,3 +5,18 @@ SELECT
       , CATEGORY 계열
       FROM TB_DEPARTMENT;
       
+--2. 학과의 학과 정원을 다음과 같은 형태로 화면에 출력한다.
+
+SELECT
+       DEPARTMENT_NAME || '의 정원은 ' || CAPACITY || '명 입니다.' as "학과별 정원"
+from  TB_DEPARTMENT;
+
+--
+--3. "국어국문학과" 에 다니는 여학생 중 현재 휴학중인 여학생을 찾아달라는 요청이
+--들어왔다. 누구인가? (국문학과의 '학과코드'는 학과 테이블(TB_DEPARTMENT)을 조회해서
+--찾아 내도록 하자)
+
+SELECT
+        TB_STUDENT
+      , TB_DEPARTMENT
+   FROM 
